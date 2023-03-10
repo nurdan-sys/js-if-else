6f05a2564b8a27ce9caaf20563e7118fc2c9295e
nurdan-sys <nurdan.alpbek@gmail.com>
2023-03-09 17:10:10 -1100

    else if

---
+++ script/script.js
@@ -0,0 +1,106 @@
+// function userCheck(user){
+//     if (user === 'admin') {
+//         return `${user} welcome`
+// }   else {
+//         return  'false not correct user'
+// }
+// }
+// console.log(userCheck('nurdan'))
+
+// function dayCheck(day){
+//     if (day <= 28) {
+//         return `${day} 'today is february'`
+// }   else {
+//         return  'on february only 28 day, your day is day of another month'
+// }
+// }
+// console.log(dayCheck(29))
+
+// function numCheck(num){
+//     if (num === 50) {
+//         return `${num} 'is equals of 50'`
+//     }   else {
+//         return  'is not correct'
+//     }
+// }
+// console.log(numCheck(40))
+
+// function ageCheck(age){
+//     if (age === 20) {
+//         return `${age} 'welcome'`
+//     }   else {
+//         return  'rejected'
+//     }
+// }
+// console.log(ageCheck(20))
+
+// function nameCheck(user){
+//     if (user.length < 3) {
+//         return `${user} 'too short'`
+//     }   else {
+//         return  'accepted'
+//     }
+// }
+// console.log(nameCheck('nu'))
+
+// function nameCheck(user){
+//     if (user.length > 5) {
+//         return `${user} 'too long'`
+//     }   else {
+//         return  'accepted'
+//     }
+// }
+// console.log(nameCheck('nurdan'))
+
+
+
+
+
+// function userAge(age){
+//     if (typeof (age) === 'number') {
+//         return `${age} 'age accepted'`
+//     }   else {
+//         return `${age} 'must be number'`
+//     }
+// }
+// console.log(userAge(33))
+
+
+// function weekDay(day){
+//     if (day === 1) {
+//         return `${day} 'today is monday'`
+// }   else if (day === 2) {
+//         return `${day} 'today is tuesday'`
+//     }else if (day === 3) {
+//         return `${day} 'today is wednesday'`
+//     }else if (day === 4) {
+//         return `${day} 'today is thursday'`
+//     }else if (day === 5) {
+//         return `${day} 'today is friday'`
+//     }else if (day === 6) {
+//         return `${day} 'today is saturday'`
+//     }else if (day === 7) {
+//         return `${day} 'today is sunday'`
+//
+//     }else {
+//         return 'max days of week 7'}
+//
+//
+//
+// }
+// console.log(weekDay(9))
+
+
+// function userAge(age){
+//     if (typeof (age) === 'number') {
+//         return `${age} 'must be string'`
+//     }   else if (age < 23) {
+//         return `${age} 'must be older'`
+//     }else{
+//         return 'accepted'
+//     }
+//
+// }
+// console.log(userAge('23'))
+
+
